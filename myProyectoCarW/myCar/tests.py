db91766d92b2d557ff7a77127ea9f6826f6d41e7
Chalo19 <gonzalobravotobar@gmail.com>
2020-10-29 16:24:09 -0300

    master

--- myProyectoCarW/myCar/tests.py
+++ myProyectoCarW/myCar/tests.py
@@ -1,21 +1,38 @@
 from django.test import TestCase
 import unittest
-from .models import MisionyVision
+from .models import Insumos,MisionyVision
 
 # Create your tests here.
+class TestInsumo(unittest.TestCase):
 
-def grabar_mision_y_vision(self):
-        m = MisionyVision(
-            mision="Garantizar la satisfacción y servicio de excelencia, ofreciendo una alternativa para maximizar el tiempo del cliente, creando la combinación perfecta entre servicio y calidad que provoque la lealtad de nuestros clientes."
-            ,vision="Ser una de las empresas más eficientes para el lavado de automóviles en Chile, ofreciendo a cada uno de nuestros clientes soluciones prácticas para el aseo de sus vehículos con la calidad, servicio y compromiso que nos caracteriza, y así poder garantizar la lealtad del consumidor."
-        )
-        valor=0
-        try:
-            m.save()
+    def grabar_insumo(self):
+
+        insum = Insumos(
+                nombre="mm", 
+                precio=199, 
+                descripcion="cualquiera", 
+                stock=200)
+        valor = 0
+        try:            
+            insum.save()
             valor = 1
         except:
             valor = 0
-        self.assertEqual(valor,1)
+        self.assertIn(valor,1)
+
+    def grabar_misionyvision(self):
+
+        vym = MisionyVision(
+                ident="unico", 
+                mision="Garantizar la satisfacción y servicio de excelencia, ofreciendo una alternativa para maximizar el tiempo del cliente, creando la combinación perfecta entre servicio y calidad que provoque la lealtad de nuestros clientes.", 
+                vision="Ser una de las empresas más eficientes para el lavado de automóviles en Chile, ofreciendo a cada uno de nuestros clientes soluciones prácticas para el aseo de sus vehículos con la calidad, servicio y compromiso que nos caracteriza, y así poder garantizar la lealtad del consumidor.")
+        valor = 0
+        try:            
+            vym.save()
+            valor = 1
+        except:
+            valor = 0
+        self.assertIn(valor,1)
 
 if __name__ == "__main__":
     unittest.main()
